fd25cb45fe8ce1ba863e59baf86b8649780b01de
Stephen <stepanfilonov@gmail.com>
2019-03-28 17:46:11 +0700

    some helping methods

--- authenticate/models.py
+++ authenticate/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models import Sum
 
 
 class Article(models.Model):
@@ -27,21 +28,28 @@ class Teams(models.Model): # team model, players(Rankings model) have foreign ke
 	def __str__(self):
 		return self.owner.username
 
+	def get_team_value(self):
+		total = []
+		total.append(self.MSplayers.all().aggregate(Sum('cost'))['cost__sum'])
+		total.append(self.WSplayers.all().aggregate(Sum('cost'))['cost__sum'])
+		total.append(self.MDplayers.all().aggregate(Sum('cost'))['cost__sum'])
+		total.append(self.WDplayers.all().aggregate(Sum('cost'))['cost__sum'])
+		total.append(self.XDplayers.all().aggregate(Sum('cost'))['cost__sum'])
+		total = [x for x in total if x is not None]
+		return sum(total)
+
+
 
 class ArchiveRecord(models.Model): # an archive record
 	team = models.ForeignKey(Teams, on_delete=models.CASCADE) # relation to a team
 	score = models.IntegerField() # score
 
 
-class PlayerTournamentRecord(models.Model): # an archive record
-	score = models.IntegerField() # score
-
-
 class Ranking(models.Model):
-	rank = models.CharField(max_length=50)
+	rank = models.IntegerField(default=10)
 	name = models.CharField(max_length=50)
 	country = models.CharField(max_length=50)
-	price = models.IntegerField(default=10) # price is a number, defaults to 10. You can set it up in the admin panel
+	cost = models.FloatField(default=10) # price is a number, defaults to 10. You can set it up in the admin panel
 	event = models.CharField(max_length=50)
 	score = models.CharField(max_length=50)
 	last = models.CharField(max_length=50)
@@ -53,31 +61,34 @@ class Ranking(models.Model):
 	player = models.SlugField()
 	picture = models.ImageField(default='default.png', blank=True)
 	playing = models.BooleanField(default=True)
-	current_tournament_score = models.ForeignKey(PlayerTournamentRecord, on_delete=models.CASCADE)
-	last_tournament_score = models.ForeignKey(PlayerTournamentRecord, on_delete=models.CASCADE, related_name='player')
+	current_tournament_score = models.IntegerField(default=10)
+	last_tournament_score = models.IntegerField(default=10)
 
 	def __str__(self):
-		return self.rank + ' ' + self.name + ' ' + str(self.playing)
+		return str(self.rank) + ' ' + self.name + ' ' + str(self.playing)
+
+	def get_type(self): # MS, WS, etc
+		return self.__class__.__name__[:2]
 
 #small subclasses for each type of player, they have all the methods from Ranking model, but also related to a team)
 
 class MSPlayer(Ranking):
-	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='MSplayers')  # connection to a team
+	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='MSplayers', null=True)  # connection to a team
 
 
 class WSPlayer(Ranking):
-	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='WSplayers')  # connection to a team
+	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='WSplayers', null=True)  # connection to a team
 
 
 class MDPlayer(Ranking):
-	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='MDplayers')  # connection to a team
+	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='MDplayers', null=True)  # connection to a team
 
 
 class WDPlayer(Ranking):
-	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='WDplayers')  # connection to a team
+	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='WDplayers', null=True)  # connection to a team
 
 
 class XDPlayer(Ranking):
-	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='XDplayers')  # connection to a team
+	team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='XDplayers', null=True)  # connection to a team
 
 
